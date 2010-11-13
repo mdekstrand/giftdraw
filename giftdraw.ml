bac495c90f92d244068bc95def9a126dc1de08f5
Michael Ekstrand <michael@elehack.net>
2010-11-13 17:57:15 -0600

    Implement e-mailing

--- giftdraw.ml
+++ giftdraw.ml
@@ -1,10 +1,28 @@
 open Batteries_uni
 open Genlex
 
+let printf = Printf.printf
+
 module M = Map.StringMap
 module S = Set.StringSet
 
 exception Cannot_match of string
+
+let mail_command = ref "sendmail -t"
+let template = ref "email.txt"
+let verbose = ref false
+let ccs = RefList.empty ()
+let commands = [
+  Arg.command ~doc:"specify mailer command" "-mailer"
+    (Arg.Set_string mail_command);
+  Arg.command ~doc:"mail template file" "-template" (Arg.Set_string template);
+  Arg.command ~doc:"be verbose (prints names)" "-v" (Arg.Set verbose);
+  Arg.command ~doc:"add a CC person" "-cc" (Arg.String (RefList.push ccs));
+]
+
+let debug fmt =
+  let print s = if !verbose then print_endline s in
+  Printf.ksprintf print fmt
 
 let rec parse_block = parser
   | [< 'Kwd "block"; 'String n1; 'String n2; blocks = parse_block >] ->
@@ -18,6 +36,45 @@ let rec parse_uspec = parser
   | [< blocks = parse_block >] ->
       [], blocks
   | [< >] -> [], []
+
+let rec draw_names igraph users people =
+  try
+    let alloc, _ = List.enum people |> fold
+        (fun (a,us) u ->
+          let blocked = M.find u igraph in
+          let candidates = S.diff us blocked in
+          if S.is_empty candidates then raise (Cannot_match u);
+          let c = S.enum candidates |> Random.choice in
+          M.add u c a, S.remove c us)
+        (M.empty, (List.enum users |> S.of_enum))
+    in alloc
+  with Cannot_match p ->
+    Printf.fprintf stderr "Cannot match %s, retrying\n%!" p;
+    draw_names igraph users people
+
+let subst params str =
+  let sparm s (n,v) =
+    let r = Str.regexp **> Str.quote ("@" ^ n ^ "@") in
+    Str.global_replace r v s
+  in
+  List.fold_left sparm str params
+
+let print_name emails (u,recip) =
+  let email = M.find u emails in
+  let text = File.with_file_in (!template) IO.read_all in
+  printf "e-mailing %s <%s>\n%!" u email;
+  debug "sending name %s -> %s" u recip;
+  let params = [
+    "GIVER", u;
+    "EMAIL", email;
+    "RECIPIENT", recip ] in
+  let text = subst params text in
+  let outch = Unix.open_process_out (!mail_command) in
+  let dispose = IO.close_out in
+  let send_mail outch = IO.output outch text 0 (String.length text) in
+  ignore (using ~dispose outch send_mail)
+
+let _ = Arg.handle commands
 
 let users, blocks = Stream.of_channel Legacy.stdin
   |> make_lexer ["person"; "block"] |> parse_uspec
@@ -34,7 +91,7 @@ let igraph = List.enum blocks |> fold
     M.add u2 (S.add u1 s2) b')
   igraph
 
-let users = 
+let users =
   let cmp u1 u2 =
     let c1 = S.cardinal (M.find u1 igraph) in
     let c2 = S.cardinal (M.find u2 igraph) in
@@ -44,22 +101,7 @@ let users =
 let rec seed_random inch =
   try Random.init (input_binary_int inch)
   with BatInnerIO.Overflow "read_i32" -> seed_random inch
-let () = File.with_file_in "/dev/urandom" seed_random
+let () = File.with_file_in "/dev/urandom" seed_random;;
 
-let rec draw_names people = 
-  try
-    let alloc, _ = List.enum people |> fold
-        (fun (a,us) u ->
-          let blocked = M.find u igraph in
-          let candidates = S.diff us blocked in
-          if S.is_empty candidates then raise (Cannot_match u);
-          let c = S.enum candidates |> Random.choice in
-          M.add u c a, S.remove c us)
-        (M.empty, (List.enum users |> S.of_enum))
-    in alloc
-  with Cannot_match p ->
-    Printf.fprintf stderr "Cannot match %s, retrying\n%!" p;
-    draw_names people
-in
-foreach (M.enum (draw_names users))
-  (fun (u,recip) -> Printf.printf "%s: %s\n" u recip)
+foreach (M.enum (draw_names igraph users users))
+  (print_name email)
